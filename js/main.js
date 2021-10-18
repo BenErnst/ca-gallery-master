841383575be30dec93e9526a16f7e00abf2f4e9f
BenErnst <benernst3@gmail.com>
2021-10-18 17:29:47 +0300

    first two projects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-console.log('Starting up');
+'use strict';
+
+
+$(onInit);
+
+
+function onInit() {
+    console.log('Starting...');
+    renderPortfolios();
+}
+
+var portfolioCount = 1;
+
+function renderPortfolios() {
+    const $portfoliosContainer = $('#portfolio .container');
+    var strHtmls = `<div class="row">
+    <div class="col-lg-12 text-center">
+    <h2 class="section-heading">Portfolio</h2>
+    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+    </div>
+    </div>
+    <div class="row">`;
+
+    strHtmls += gProjs.map(proj => {
+        return `<div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${portfolioCount}">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content">
+        <i class="fa fa-plus fa-3x"></i>
+        </div>
+        </div>
+        <img class="img-fluid" src="img/portfolio/${proj.id}.png" alt="">
+        </a>
+        <div class="portfolio-caption">
+        <h4>${proj.title}</h4>
+        <p class="text-muted">${proj.desc}</p>
+        </div>
+        </div>`;
+    })
+
+    // strHtmls = strHtmls.join('')
+    $portfoliosContainer.html(strHtmls);
+}
---
+++ js/services/gallery-service.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+
+
+const gProjs = [
+    {
+        id: "minesweeper",
+        name: "Minesweeper",
+        title: "Minesweeper",
+        desc: "Sprint 1 Project",
+        url: "https://benernst.github.io/minesweeper/",
+        publishedAt: 1633551810,
+        labels: ["Negs", "right-click events"]
+    },
+    {
+        id: "pacman",
+        name: "Pacman",
+        title: "Pacman",
+        desc: "Pacman Game Project",
+        url: "https://benernst.github.io/pacman/",
+        publishedAt: 1633284765,
+        labels: ["Imgs rendering", "keyboard events"]
+    }
+]
+
+
+
+
+
